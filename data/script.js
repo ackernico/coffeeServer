a0b3f5734155c0850dc7d0f566cb60b0d8af0f44
ackernico <ackernicolas0@gmail.com>
2025-08-24 15:07:37 -0300

    feature: added inital homepage layout and changed color palette

--- data/script.js
+++ data/script.js
@@ -1,10 +1,42 @@
-const slides = document.querySelectorAll('.slide');
+const sections = document.querySelectorAll('.sections');
 const getActive = document.getElementById('active');
+const methodButton = document.getElementById('method');
 const sectionContent = 
 {
-    power: `
+    home: `
+    <div id="upText">
         <h2>Homepage</h2>
-        <h1>I am the number one rap of artist generation</h1>
+    </div>    
+    <div id="sectionContent">
+        <div id="powerSection">
+                <h2 >Power</h2>
+                <button>ON</button>
+                <input class="slider" type="range" min="1" max="23">
+                <h2>Saved profile</h2>
+                <button>
+                    <p id="method">Melitta</p>
+                </button>
+        </div>
+        <div id="recentTable">
+            <table>
+                <tr>
+                    <th>Date</th>
+                    <th>Duration</th>
+                    <th>Method</th>
+                </tr>
+                <tr>
+                    <th>04. dez</th>
+                    <th>2:40</th>
+                    <th>Press</th>
+                 </tr>
+                 <tr>
+                    <th>02. jan</th>
+                    <th>1:50</th>
+                    <th>V60</th>
+                </tr>
+            </table>
+        </div>
+    </div>
     `,
     schedule: `
         <h1>I am shakespeare in the flesh</h1>
@@ -22,15 +54,24 @@ const sectionContent =
 
 document.addEventListener('DOMContentLoaded', () =>
 {
-    document.getElementById('power').classList.add('active');
-    getActive.innerHTML = sectionContent['power'];
+    document.getElementById('home').classList.add('active');
+    document.getElementById('display-container').innerHTML = sectionContent['home'];
 });
 
-slides.forEach(slide =>
+sections.forEach(sec =>
 {
-    slide.addEventListener('click', () => {
-        slides.forEach(j => j.classList.remove('active'));
-        slide.classList.add('active');
-        getActive.innerHTML = sectionContent[slide.id];
+    sec.addEventListener('click', () => {
+        sections.forEach(j => j.classList.remove('active'));
+        sec.classList.add('active');
+        document.getElementById('display-container').innerHTML = sectionContent[sec.id];
     });
 });
+
+
+/* Test function
+document.getElementById('display-container').addEventListener('click', (e) => {
+    if (e.target.id === 'method') {
+        e.target.innerHTML = "Espresso";
+    }
+});
+*/ 
